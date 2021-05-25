b6f9514f5a3d00368214b7cf94829563bb310dfc
Grefgg <grefgg@Sineads-MacBook-Pro-2.local>
2021-05-25 13:51:08 +0100

    itterows function used

--- worldcupdata.py
+++ worldcupdata.py
@@ -16,11 +16,40 @@ import seaborn as sns
 import requests
 import json
 
-#importing csv file world cup dataset from kaggle.
+#importing csv file world cup dataset from kaggle and creating a dataframe.
 worldcup_data = '/Users/grefgg/Documents/Data/WorldCupMatches.csv'
-worldcup_matches = pd.read_csv(worldcup_data)
-print(worldcup_matches.head())
-print(sns.pairplot(worldcup_matches))
-sns.load_dataset("worldcup_data")
-print(sns.heatmap(worldcup_matches.corr()))
+world_cup_players = '/Users/grefgg/Documents/Data/WorldCupPlayers.csv'
+worldcup_matches_df = pd.read_csv(worldcup_data)
+worldcup_matches_df = pd.read_csv(world_cup_players)
 
+# Analysing data/ Cleaning data
+
+print(worldcup_matches_df.head())
+print(worldcup_matches_df.info())
+
+# Replace missing values or dropping duplicates
+print(worldcup_matches_df.isnull().sum())
+print(worldcup_matches_df.head())
+
+# Your project should include sorting, indexing, grouping.
+new_index = worldcup_matches_df.set_index('RoundID')
+new_index ["Position"].fillna(method = 'ffill',inplace= True)
+new_index ["Event"].fillna(method = 'ffill',inplace= True)
+new_index.drop_duplicates(subset= None, keep='first', inplace= False)
+print (new_index.head())
+teams = new_index.groupby('Team Initials')
+coaches = new_index.groupby('Coach Name')
+print(teams.first())
+print(coaches.head())
+
+
+# Slicing, loc or iloc.
+
+# Looping, iterrows 
+next(new_index.iterrows())[3:]
+
+# Merge DataFrames 
+
+
+
+#	Define a custom function to create reusable code. 
